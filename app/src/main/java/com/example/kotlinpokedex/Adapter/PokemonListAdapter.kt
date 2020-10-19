c9041f0ae3a30ce466dc03c1198e90b074ba3ed8
Sukhdip <=>
2020-10-18 21:12:44 -0700

    update ui of the main list

--- app/src/main/java/com/example/kotlinpokedex/Adapter/PokemonListAdapter.kt
+++ app/src/main/java/com/example/kotlinpokedex/Adapter/PokemonListAdapter.kt
@@ -2,10 +2,13 @@ package com.example.kotlinpokedex.Adapter
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +16,7 @@ import com.bumptech.glide.Glide
 import com.example.kotlinpokedex.Common.Common
 import com.example.kotlinpokedex.Model.Pokemon
 import com.example.kotlinpokedex.R
+import com.robertlevonyan.views.chip.Chip
 
 class PokemonListAdapter(
     internal var context: Context,
@@ -22,10 +26,18 @@ class PokemonListAdapter(
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         internal var pokemonImage: ImageView
         internal var pokemonName: TextView
+        internal var pokemonId: TextView
+        internal var linearLayout: LinearLayout
+        internal var type_chip_one: Chip
+        internal var type_chip_two: Chip
 
         init {
             pokemonImage = itemView.findViewById(R.id.pokemon_image) as ImageView
             pokemonName = itemView.findViewById(R.id.pokemon_name) as TextView
+            pokemonId = itemView.findViewById(R.id.pokemon_id) as TextView
+            linearLayout = itemView.findViewById(R.id.linear_layout_card_background) as LinearLayout
+            type_chip_one = itemView.findViewById(R.id.chip_one) as Chip
+            type_chip_two = itemView.findViewById(R.id.chip_two) as Chip
         }
 
         fun setItemClickListener(onClickListener: View.OnClickListener) {
@@ -48,10 +60,24 @@ class PokemonListAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         Glide.with(context).load(pokemonList[position].img).into(holder.pokemonImage)
         holder.pokemonName.text = pokemonList[position].name
+        holder.pokemonId.text = "#${pokemonList[position].id.toString().padStart(3, '0')}"
+
+        holder.type_chip_one.chipText = pokemonList[position].type[0]
+
+        if (pokemonList[position].type.size == 2) {
+            holder.type_chip_two.chipText = pokemonList[position].type[1]
+            holder.type_chip_two.visibility = View.VISIBLE
+        } else {
+            holder.type_chip_two.visibility = View.INVISIBLE
+        }
+
         holder.setItemClickListener(View.OnClickListener {
             LocalBroadcastManager.getInstance(context)
                 .sendBroadcast(Intent(Common.KEY_ENABLE_HOME).putExtra("position", position))
         })
+
+        holder.linearLayout.setBackgroundColor(Common.getColorByType(pokemonList[position].type[0]))
+
     }
 
 }
