60d1f7ee1726744bf7ee1effdb416e5530587957
Sukhdip <=>
2020-10-18 17:10:44 -0700

    add click listener of items and open pokemon details fragment

--- app/src/main/java/com/example/kotlinpokedex/Adapter/PokemonListAdapter.kt
+++ app/src/main/java/com/example/kotlinpokedex/Adapter/PokemonListAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.kotlinpokedex.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.kotlinpokedex.Common.Common
 import com.example.kotlinpokedex.Model.Pokemon
 import com.example.kotlinpokedex.R
 
@@ -25,6 +28,11 @@ class PokemonListAdapter(
             pokemonName = itemView.findViewById(R.id.pokemon_name) as TextView
         }
 
+        fun setItemClickListener(onClickListener: View.OnClickListener) {
+            itemView.setOnClickListener { view ->
+                onClickListener.onClick(view)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -40,6 +48,10 @@ class PokemonListAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         Glide.with(context).load(pokemonList[position].img).into(holder.pokemonImage)
         holder.pokemonName.text = pokemonList[position].name
+        holder.setItemClickListener(View.OnClickListener {
+            LocalBroadcastManager.getInstance(context)
+                .sendBroadcast(Intent(Common.KEY_ENABLE_HOME).putExtra("position", position))
+        })
     }
 
 }
--- app/src/main/java/com/example/kotlinpokedex/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpokedex/MainActivity.kt
@@ -1,10 +1,44 @@
 package com.example.kotlinpokedex
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.kotlinpokedex.Common.Common
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Create broadcast handle
+    private val showDetail = object:BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent!!.action!!.toString() == Common.KEY_ENABLE_HOME) {
+                supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+                supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+                // Replace Fragment
+                val detailFragment = PokemonDetail.getInstance()
+                val position = intent.getIntExtra("position", -1)
+                val bundle = Bundle()
+                bundle.putInt("position", position)
+                detailFragment.arguments = bundle
+
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.list_pokemon_fragment, detailFragment)
+                fragmentTransaction.addToBackStack("detail")
+                fragmentTransaction.commit()
+
+                val pokemon = Common.pokemonList[position]
+                toolbar.title = pokemon.name
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +47,30 @@ class MainActivity : AppCompatActivity() {
         toolbar.title = "Pokedex"
         setSupportActionBar(toolbar)
 
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(showDetail, IntentFilter(Common.KEY_ENABLE_HOME))
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId)
+        {
+            android.R.id.home -> {
+                toolbar.title = "Pokemon List"
+                supportFragmentManager.popBackStack("detail", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                supportActionBar!!.setDisplayShowHomeEnabled(false)
+                supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+            }
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        toolbar.title = "Pokemon List"
+//        supportFragmentManager.popBackStack("detail", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        supportActionBar!!.setDisplayShowHomeEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+    }
+
 }
